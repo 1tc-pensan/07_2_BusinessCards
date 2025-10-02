eef4c4e1ddcd1ae3b32971885348834b2519a23c
1t-pensan <ppatrik0319@gmail.com>
2025-10-02 10:33:52 +0200

    class+construct

--- App/Models/Person.php
+++ App/Models/Person.php
@@ -1,3 +1,17 @@
 <?php
-
+namespace App\Models;
+class Person
+{
+    protected ?int $id;
+    public string $name;
+    protected string $email;
+    private string $phone;
+    public function _construct(?$id,string $name,string $email,string $phone)
+    {
+        $this->id=$id;
+        $this->name=$name;
+        $this->email=$email;
+        $this->phone=$phone;
+    }
+}
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="https://cdn.simplecss.org/simple-v1.css">
 <?php
 // Egyszerű autoloader a kód betöltéséhez a mappaszerkezet alapján.
 spl_autoload_register(function ($class) {
