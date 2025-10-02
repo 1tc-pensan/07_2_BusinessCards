917300f058f09971b94ffd43b2283be39f7475aa
1t-pensan <ppatrik0319@gmail.com>
2025-10-02 10:39:08 +0200

    class+construct

--- App/Models/Person.php
+++ App/Models/Person.php
@@ -6,7 +6,7 @@ class Person
     public string $name;
     protected string $email;
     private string $phone;
-    public function _construct(?$id,string $name,string $email,string $phone)
+    public function __construct(?$id,string $name,string $email,string $phone)
     {
         $this->id=$id;
         $this->name=$name;
